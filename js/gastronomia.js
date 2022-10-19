b692883a302c11ee36d1c9954744bd6619c4ea24
Joaquin Parada <joacoparada96@gmail.com>
2022-10-19 10:12:34 -0300

    anchors a sitios web de los lugares de gastronomia en gastronomia.js

--- js/gastronomia.js
+++ js/gastronomia.js
@@ -7,34 +7,33 @@ fetch("../gastronomia.json")
 function traerGastronomia(data) {
   let gastronomia = "";
   for (let el of data) {
-    console.log(el.lugares[1][1]);
     let gastronomiaHTML = `
         <article class="gastronomia-article">
           <h2 class="gastronomia-titulo">${el.comida}</h2>
           <p class="texto-gastronomia">${el.descripcion}</p>
           <div class="gastronomia-info-container">
             <ul class="ul-gastronomia">
-              <li><b class="gastronomia-local-titulo">${el.lugares[0][0]}</b>
+              <li><b class="gastronomia-local-titulo"><a href=${el.lugares[0][2]} target="_blank">${el.lugares[0][0]}</a></b>
                 <ul>
                   <li class="local-direccion">${el.lugares[0][1]}</li>
                 </ul>
               </li>
-              <li><b class="gastronomia-local-titulo">${el.lugares[1][0]}</b>
+              <li><b class="gastronomia-local-titulo"><a href=${el.lugares[1][2]} target="_blank">${el.lugares[1][0]}</a></b>
                 <ul>
                   <li class="local-direccion">${el.lugares[1][1]}</li>
                 </ul>
               </li>
-              <li><b class="gastronomia-local-titulo">${el.lugares[2][0]}</b>
+              <li><b class="gastronomia-local-titulo"><a href=${el.lugares[2][2]} target="_blank">${el.lugares[2][0]}</a></b>
                 <ul>
                   <li class="local-direccion">${el.lugares[2][1]}</li>
                 </ul>
               </li>
-              <li><b class="gastronomia-local-titulo">${el.lugares[3][0]}</b>
+              <li><b class="gastronomia-local-titulo"><a href=${el.lugares[3][2]} target="_blank">${el.lugares[3][0]}</a></b>
                 <ul>
                   <li class="local-direccion">${el.lugares[3][1]}</li>
                 </ul>
               </li>
-              <li><b class="gastronomia-local-titulo">${el.lugares[4][0]}</b>
+              <li><b class="gastronomia-local-titulo"><a href=${el.lugares[4][2]} target="_blank">${el.lugares[4][0]}</a></b>
                 <ul>
                   <li class="local-direccion">${el.lugares[4][1]}</li>
                 </ul>
