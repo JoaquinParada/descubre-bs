0f6a259eed506c25f0cb955f2e9792743456c618
Joaquin Parada <joacoparada96@gmail.com>
2022-10-20 19:41:58 -0300

    se agrego la api formspree al formulario y el fetch en contact.js

--- js/contact.js
+++ js/contact.js
@@ -6,15 +6,15 @@ let bar3 = document.querySelector(".bar-3");
 
 btnHam.addEventListener("click", slideNav);
 
-function slideNav(){
+function slideNav() {
   nav.classList.toggle("active");
 
-  if(nav.classList.contains("active")){
+  if (nav.classList.contains("active")) {
     bar1.classList.add("bar-active-1");
     bar2.classList.add("bar-active-2");
     bar3.classList.add("bar-active-3");
   }
-  else{
+  else {
     bar1.classList.remove("bar-active-1");
     bar2.classList.remove("bar-active-2");
     bar3.classList.remove("bar-active-3");
@@ -23,39 +23,60 @@ function slideNav(){
 
 }
 
+//Funcion Formulario
+let inputNombre = document.querySelector("#nombre");
+let inputMail = document.querySelector("#email");
 
-function validacion() 
-{
-    // VALIDACION NOMBRE
-    valor = document.getElementById("nombre").value;
-    if( valor == null || valor.length == 0) 
-    {
-      // alert("Ingrese Nombre!!");
-        // document.getElementById('mensaje').innerHTML = "Complete el Nombre!";
-        elemento = document.getElementById('nombre');
-        elemento.focus();
-        
-        alert("Ingrese Nombre!!");
-        return false;
-        
-    }
+let form = document.querySelector('#form');
+form.addEventListener('submit', handleSubmit);
 
-    // VALIDACION EMAIL
-    valor = document.getElementById("email").value;
-    
-      
-    if (!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|ar)+$/.test(valor)))
-    {
-        //document.getElementById('mensaje').innerHTML = "No es una direccion de email correcta" ;
-        
-        elemento = document.getElementById('email');
-        elemento.value = '';
-        elemento.focus();
-        alert("Ingrese Email!!");
-        return false;
+async function handleSubmit(event) {
+  event.preventDefault();
+  if(validacion()){
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+      method: this.method,
+      body: form,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+  
+    if (response.ok) {
+      this.reset()
+      alert('Respuesta API - Gracias por contactarme')
     }
-        
-             
-    alert("Gracias por compeltar sus datos!!");
-    return true;          
+    inputNombre.value= "";
+    inputMail.value="";
+  }
+  else{
+    return;
+  }
+}
+
+
+function validacion() {
+  // VALIDACION NOMBRE
+  let valorNombre = inputNombre.value;
+  if (valorNombre == null || valorNombre.length == 0) {
+    let elemento = document.getElementById('nombre');
+    elemento.focus();
+    alert("Ingrese Nombre!!");
+    return false;
+  }
+
+  // VALIDACION EMAIL
+  let valorMail = inputMail.value;
+
+  if (!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|ar)+$/.test(valorMail))) {
+    //document.getElementById('mensaje').innerHTML = "No es una direccion de email correcta" ;        
+    let elementoMail = document.getElementById('email');
+    elementoMail.value = '';
+    elementoMail.focus();
+    alert("Ingrese Email!!");
+    return false;
+  }
+
+  alert("Gracias por compeltar sus datos!!");
+  return true;
 }
